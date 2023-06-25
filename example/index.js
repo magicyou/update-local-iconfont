179f019d276d8dc19bafbb02ffe7c77f3e7fa9e8
lixiaolong <lixl@anpro-tech.com>
2023-06-25 18:23:31 +0800

    [feat] 优化文件夹同步策略，删除改为覆盖写入

--- example/index.js
+++ example/index.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const update = new UpdateIconfont(
   {
-    url: '',
+    url: 'https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7543c303&pid=3647633&ctoken=rAjSk22DfprV5TN3fwz-MaZC',
     cookie: '',
-    output: './src/assets/iconfont',
+    output: path.join(__dirname, './src/assets/iconfont' ),
     runDir: path.join(__dirname, './')
   }
 )
